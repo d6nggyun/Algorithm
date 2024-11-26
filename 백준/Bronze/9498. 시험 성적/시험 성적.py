dc67968f968dbe233169217ac1c7a428396f94bc
김동균 <165386023+d6nggyun@users.noreply.github.com>
2024-11-26 14:31:23 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+g = int(input())
+if g <= 100 and g >= 90:
+    print("A")
+elif g <= 89 and g >= 80:
+    print("B")
+elif g <= 79 and g >= 70:
+    print("C")
+elif g <= 69 and g >= 60:
+    print("D")
+else:
+    print("F")
