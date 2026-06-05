73d5aa46622cdb6f11d56db3bbaa99a4381a1023
김동균 <165386023+d6nggyun@users.noreply.github.com>
2026-06-05 20:02:33 +0900

    [level 2] Title: 올바른 괄호, Time: 9.44 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,23 +2,28 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        List<Character> stack = new ArrayList<>();
+        // 스택을 사용해서 열린 괄호를 만나면 닫힌 괄호를 만날 때 pop 되도록
+        // 마지막 문자까지 돌은 후, 스택에 괄호가 아직 남아있을 경우, false를 반환하도록
+        Stack<Character> stack = new Stack<>();
         
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+            Character c = s.charAt(i);
             
             if (c == '(') {
-                stack.add(c);
-            } else if (c == ')') {
-                if (stack.contains('(')) {
-                    stack.remove(Character.valueOf('('));
-                } else {
-                    stack.add(c);
-                }
+                stack.push(c);
+            }
+            else if (c == ')') {
+                // 만약 닫힌 괄호 차례인데, 스택이 비었다면 return false
+                if (stack.isEmpty()) return false;
+                else stack.pop();
+            }
+            else {
+                System.out.println("Wrong Input");
             }
         }
         
-        if (stack.isEmpty()) return true;
-        else return false;
+        if (!stack.isEmpty()) return false;
+        
+        return true;
     }
 }
